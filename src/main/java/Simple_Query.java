05866de4b7245d8d8c88a4f895d3b11aa1b6b678
Votre nom ou pseudo <mathiasali@outlook.com>
2018-11-17 20:09:18 +0100

    First commit

---
+++ src/main/java/Simple_Query.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+//import com.mysql.jdbc.Driver;
+
+public class Simple_Query {
+    public static void main(String[] args) throws Exception {
+        try {
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+            } catch (ClassNotFoundException nf) {
+                nf.printStackTrace();
+            }
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:8888/sakila",
+                "root", "root");
+            System.out.println("here 0");
+
+            Statement stmt = conn.createStatement();
+
+            System.out.println("here 1");
+
+            ResultSet rs = stmt.executeQuery("SELECT last_name from actor;");
+            int rowID = 1;
+            while (rs.next()) {
+                // print the values for the current row.
+                String current_last_name = rs.getString("last_name");
+                System.out.println("ROW = " + rowID + ", last name =  " + current_last_name);
+                rowID++;
+            }
+
+            rs.close();
+            stmt.close();
+
+            System.out.println("here 2");
+        } catch (SQLException ex) {
+            System.out.println("Error : " + ex);
+        }
+
+
+    }
+}
